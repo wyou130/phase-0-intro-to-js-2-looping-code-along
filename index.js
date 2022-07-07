2bb78bfea6c9a146d53726a87fda8e61e4e2c202
Wendy You <wyou130@gmail.com>
2022-07-07 00:16:47 -0400

    commit tree daee0316159d9f65910b14fb4c2944e3ff93e921

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const events = "birthday"
+const dogs = ["Wallace", "Sniff", "Charlie", "Perr"]
+
+function writeCards(dogs, events) {
+    let cards =[]
+    for (let i = 0; i < dogs.length; i++) {
+        cards.push(`Thank you, ${dogs[i]}, for the wonderful ${events} gift!`)
+    }
+    return cards
+}
+
+writeCards(dogs, events)
+
+function countDown(integer) {
+    let x = 10
+    while (x >= 0) {
+        console.log(x)
+        x--
+    }
+    return integer
+}
+
+countDown(integer)
